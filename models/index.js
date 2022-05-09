b0bcf7c0ec3c4574fa595afd11eedc67f59e289b
Hugo530 <101437827+Hugo530@users.noreply.github.com>
2022-05-09 11:22:26 -0700

    money model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
 const User = require('./User');
 const Pigs = require('./Pigs');
+<<<<<<< Updated upstream
+=======
+const Money = require('./Money');
+>>>>>>> Stashed changes
 
 module.exports = {
   User,
