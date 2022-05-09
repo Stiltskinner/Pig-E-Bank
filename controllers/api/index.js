c92dfa26a9423a027718eb21356351e31b2ac99e
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-05-09 14:06:22 -0500

    Created api route for pigs with a get all, updated api index in controller to work with api pigs routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const pigsRoutes = require('./pigs-routes.js');
 
 router.use('/user', userRoutes);
+router.use('/pigs', pigsRoutes);
 
 module.exports = router;
