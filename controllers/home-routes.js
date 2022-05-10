361d819cd9b80970cc4104d8fe1a99a1cf0e0b11
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-05-10 00:55:50 -0500

    handlebars is rolling, introducing variables and pigs into the main dashboard view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
-const {  } = require('../models/');
+const { Pigs, User } = require('../models/');
 
 // THIS IS JUST FOR TESTING PURPOSES IT WILL CHANGE IN THE FUTURE
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', async (req, res) => {
+  const userData = await User.findByPk(1, {
+    where: {
+      id: 1
+    },
+})
+  const pigData = await Pigs.findAll({
+    where: {
+      user_id: 1
+    },
+  });
+
+  const pigs = pigData.map((pig) => pig.get({ plain: true }));
+  const user = userData.get({ plain: true });
   res.render('main-view', {
     layout: 'dashboard',
+    pigs,
+    user
   });
 });
+// END OF TESTER
 
 router.get('/login', (req, res) => {
   
