aab342e19c8119a2659fdc42e441c649607d7b3f
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-05-10 14:48:34 -0500

    newseeds partially setup

--- seeds/money-seed.js
+++ seeds/money-seed.js
@@ -5,6 +5,26 @@ const moneyData = [
     savings: 1000.00,
     checking: 2000,
     user_id: 1,
+  },
+  {
+    savings: 787.00,
+    checking: 1534.32,
+    user_id: 2,
+  },
+  {
+    savings: 124521.23,
+    checking: 875476.23,
+    user_id: 3,
+  },
+  {
+    savings: 63451.30,
+    checking: 5315,
+    user_id: 4,
+  },
+  {
+    savings: 1200.51,
+    checking: 1300,
+    user_id: 5,
   }
 ];
 
--- seeds/pig-seed.js
+++ seeds/pig-seed.js
@@ -2,16 +2,16 @@ const { Pigs } = require('../models');
 
 const pigsData = [
   {
-    name: 'test_pig',
+    name: 'rent',
     is_savings: true,
-    amount_due: 200,
-    amount_held: 300,
+    amount_due: 800,
+    amount_held: 900,
     direct_deposit: '2022-05-10',
     due_date: '2022-05-30',
     user_id: 1
   },
   {
-    name: 'Larry the pig',
+    name: 'utilities',
     is_savings: true,
     amount_due: 100,
     amount_held: 200,
@@ -20,15 +20,55 @@ const pigsData = [
     user_id: 1
   },
   {
-    name: 'Sheila the Pig',
+    name: 'food',
+    is_savings: true,
+    amount_held: 400,
+    direct_deposit: '2022-05-03',
+    user_id: 1
+  },
+  {
+    name: 'entertainment',
     is_savings: true,
     amount_due: 400,
     amount_held: 400,
     direct_deposit: '2022-05-03',
-    due_date: '2022-05-29',
     user_id: 1
   },
+  {
+    name: 'rent',
+    is_savings: true,
+    amount_due: 800,
+    amount_held: 900,
+    direct_deposit: '2022-05-10',
+    due_date: '2022-05-30',
+    user_id: 2
+  },
+  {
+    name: 'utilities',
+    is_savings: true,
+    amount_due: 100,
+    amount_held: 200,
+    direct_deposit: '2022-06-10',
+    due_date: '2022-06-30',
+    user_id: 2
+  },
+  {
+    name: 'food',
+    is_savings: true,
+    amount_held: 400,
+    direct_deposit: '2022-05-03',
+    user_id: 2
+  },
+  {
+    name: 'entertainment',
+    is_savings: true,
+    amount_due: 400,
+    amount_held: 400,
+    direct_deposit: '2022-05-03',
+    user_id: 2
+  },
 ];
+
 
 const seedPigs = () => Pigs.bulkCreate(pigsData);
 
