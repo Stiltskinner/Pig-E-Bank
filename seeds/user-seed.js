02e6bea19d4c8b52e470e691b3abbdcf77c69236
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-05-10 14:53:16 -0500

    seeds ready to go

--- seeds/user-seed.js
+++ seeds/user-seed.js
@@ -2,7 +2,33 @@ const { User } = require('../models');
 
 const userData = [
   {
+    first_name: "Johnny",
+    last_name: "Moneymaker",
     email: 'Test@test.com',
+    password: 'password123',
+  },
+  {
+    first_name: "Sarah",
+    last_name: "Silver",
+    email: 'Sarah@test.com',
+    password: 'password123',
+  },
+  {
+    first_name: "Tom",
+    last_name: "Cruise",
+    email: 'Tom@test.com',
+    password: 'password123',
+  },
+  {
+    first_name: "Josh",
+    last_name: "Jackson",
+    email: 'Josh@test.com',
+    password: 'password123',
+  },
+  {
+    first_name: "Laila",
+    last_name: "Jackson",
+    email: 'Laila@test.com',
     password: 'password123',
   },
 ];
