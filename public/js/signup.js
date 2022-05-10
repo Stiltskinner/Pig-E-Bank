5ec4ea2f31a5398ca823d09ab076ee201213e42e
Rene Vasquez <rene4801@gmail.com>
2022-05-10 14:47:48 -0500

    Fixed CSS and started on Public JS

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,22 @@
 const signupFormHandler = async function(event) {
   event.preventDefault();
 
-  const usernameEl = document.querySelector('#username-input-signup');
-  const passwordEl = document.querySelector('#password-input-signup');
+  const firstNameEl = document.querySelector('#firstNameInput');
+  const lastNameEl = document.querySelector('#lastNameInput');
+  const emailInputEl = document.querySelector('#emailInput');
+  const verifyEmailInputEl = document.querySelector('#verifyEmailInput');
+  const passwordInputEl = document.querySelector('#passwordInput');
+  const verifyPasswordInputEl = document.querySelector('#verifyPasswordInput');
 
   const response = await fetch('/api/user/', {
     method: 'POST',
     body: JSON.stringify({
-      username: usernameEl.value,
-      password: passwordEl.value,
+      firstName: firstNameEl.value,
+      lastName: lastNameEl.value,
+      emailInput: emailInputEl.value,
+      verifyEmailInput: verifyEmailInputEl.value,
+      passwordInput: passwordInputEl.value,
+      verifyPasswordInput: verifyPasswordInputEl.value,
     }),
     headers: { 'Content-Type': 'application/json' },
   });
