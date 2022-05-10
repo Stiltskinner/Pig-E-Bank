38f8d4675f98cde309875a3d1dc42fa59800fccf
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-05-10 11:55:57 -0500

    Created all api routes for money

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const pigsRoutes = require('./pigs-routes.js');
+const moneyRoutes = require('./money-routes.js');
 
 router.use('/user', userRoutes);
 router.use('/pigs', pigsRoutes);
+router.use('/money', moneyRoutes);
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -27,7 +27,6 @@ router.post('/login', async (req, res) => {
         email: req.body.email,
       },
     });
-console.log("user info", user)
     if (!user) {
       res.status(400).json({ message: 'No user account found!' });
       return;
