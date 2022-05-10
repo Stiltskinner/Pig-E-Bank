ca9458775ae6a819140f72be4ef92ed3e5be7f28
Hugo530 <101437827+Hugo530@users.noreply.github.com>
2022-05-10 12:46:59 -0700

    added first and last name

--- models/User.js
+++ models/User.js
@@ -18,6 +18,14 @@ User.init(
       primaryKey: true,
       autoIncrement: true
     },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
