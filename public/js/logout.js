babdf2c8a0228466d12232f66882be6f901582e1
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-05-11 11:45:39 -0500

    finishing up pig details

--- public/js/logout.js
+++ public/js/logout.js
@@ -11,4 +11,4 @@ const logout = async function() {
   }
 };
 
-document.querySelector('#logout-link').addEventListener('click', logout);
+document.querySelector('#signOut').addEventListener('click', logout);
