b6af1b2e54b488728346edb061dd3ce31426cf52
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-05-11 14:53:41 -0500

    feedroutes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Pigs, User, Money, Transaction  } = require('../models/');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     console.log(req.session.userId)
     if(req.session.userId) {
         const userData = await User.findByPk(req.session.userId, {
@@ -19,5 +19,47 @@ router.get('/', async (req, res) => {
     res.redirect('login');
     return;
   });
+
+router.get('/savings', withAuth, async (req, res) => {
+  console.log(req.session.userId)
+  if(req.session.userId) {
+      const userData = await User.findByPk(req.session.userId, {
+          include: [{ model: Pigs}, { model: Money }, { model: Transaction }],
+          where: {
+            is_savings: true,
+          }
+        })
+        const user = userData.get({ plain: true });
+        console.log(user)
+        res.render('accounts', {
+          layout: 'dashboard',
+          user
+        });
+        return;
+  }
+  res.redirect('login');
+  return;
+});
+
+router.get('/checking', withAuth, async (req, res) => {
+  console.log(req.session.userId)
+  if(req.session.userId) {
+      const userData = await User.findByPk(req.session.userId, {
+          include: [{ model: Pigs}, { model: Money }, { model: Transaction }],
+          where: {
+            is_savings: false,
+          }
+        })
+        const user = userData.get({ plain: true });
+        console.log(user)
+        res.render('accounts', {
+          layout: 'dashboard',
+          user
+        });
+        return;
+  }
+  res.redirect('login');
+  return;
+});
 
 module.exports = router;
