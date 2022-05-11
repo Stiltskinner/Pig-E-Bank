c7b234507b011762c1f409614788063630f4acc9
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-05-11 10:23:48 -0500

    Fixed transaction seeds

--- seeds/transaction-seed.js
+++ seeds/transaction-seed.js
@@ -3,6 +3,7 @@ const { Transaction } = require('../models');
 const transactionData = [
   {
     transaction_amt: 400.00,
+    merchant: "Amazon",
     user_id: 1,
     pigs_id: 1,
     is_savings: false,
@@ -10,6 +11,7 @@ const transactionData = [
   },
   {
     transaction_amt: 200.00,
+    merchant: "Wal-Mart",
     user_id: 1,
     pigs_id: 2,
     is_savings: false,
@@ -17,6 +19,7 @@ const transactionData = [
   },
   {
     transaction_amt: 100.00,
+    merchant: "City of Austin",
     user_id: 1,
     pigs_id: 3,
     is_savings: false,
@@ -24,6 +27,7 @@ const transactionData = [
   },
   {
     transaction_amt: 500.00,
+    merchant: "Amazon",
     user_id: 1,
     pigs_id: 4,
     is_savings: false,
